2725c97a7746efdf994ff0d2994f3cf88be776ef
jeff <12181410+nondevops@users.noreply.github.com>
2022-07-15 11:45:02 +0800

    新增对tech等后缀主域检测

--- collect_domain_expiretime.sh
+++ collect_domain_expiretime.sh
@@ -17,26 +17,28 @@ domain_list=(
 baidu.com
 weibo.com
 51cto.com
+nic.top
+baidu.tech
+baidu.la
 )
 
 function check_domain() {
   domain=$1
   ts=$(date +%s)
   localip=$(/usr/sbin/ifconfig `/usr/sbin/route|grep '^default'|awk '{print $NF}'`|grep inet|awk '{print $2}'|head -n 1)
-
-  #ping -c1 223.5.5.5 &> /dev/null
+  timeout_time="timeout 30 "
+  ping -c1 223.5.5.5 &> /dev/null
   if [ $? -eq 0 ];then
     domain_tmp=`echo $domain |cut -d '.' -f 2`
-
     if [ "$domain_tmp" == "com" ];then
         # whois baidu.com | grep "Expiration Date" |awk '{print $5}' | cut -c1-10 | awk -F '-' '{print $1 $2 $3}'
-        expire_date=`whois $domain | grep "Expiration Date" |awk '{print $5}' | cut -c1-10 | awk -F '-' '{print $1 $2 $3}'`
+        expire_datea=`${timeout_time} whois $domain | grep "Expiration Date" |awk '{print $5}' | cut -c1-10 | awk -F '-' '{print $1 $2 $3}'`
 
-    elif [ "$domain_tmp" == "cn" ];then
-        expire_date=`whois $domain | grep "Expiration Time" | awk '{print $3}' | awk -F '-' '{print $1 $2 $3}'`
+    elif [ "$domain_tmp" == "cn" -o "$domain_tmp" == "com.cn" ];then
+        expire_date=`${timeout_time} whois $domain | grep "Expiration Time" | awk '{print $3}' | awk -F '-' '{print $1 $2 $3}'`
 
-    elif [ "$domain_tmp" == "la" ];then
-        expire_date=`whois $domain | grep 'Expiry Date' | awk '{print $4}'|cut -d 'T' -f 1`
+    elif [ "$domain_tmp" == "la" -o "$domain_tmp" == "top" -o  "$domain_tmp" == "tech" ];then
+        expire_date=`${timeout_time} whois $domain | grep 'Expiry Date' | awk '{print $4}'|cut -d 'T' -f 1`
     fi
 
     END_TIME_STAMP=$(date +%s -d "${expire_date}") 
@@ -44,7 +46,7 @@ function check_domain() {
     domain_expire_days=$[$[$END_TIME_STAMP-$NOW_TIME_STAMP]/86400]
     metrics="domain,localip=${localip},domain_name=${domain} expire_days=${domain_expire_days}"
     echo "$metrics"
-    fi
+  fi
 
 }
 
